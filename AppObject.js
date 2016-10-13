11f69b4db650abd3e442de20f83daf252b95a06b
EGAguirre <9eaguirre@stpatrick.org>
2016-10-13 10:26:28 -0500

    Filled in the blanks
    -added conditional statement for function sleep
    -added in the instance functions
    - added in all the instance variables
    -

---
+++ AppObject.js
@@ -0,0 +1,52 @@
+function App(n,m,s){
+
+    //Declare your instance variables here.
+    this.name = n;
+    this.memory = m;
+    this.state = s;
+
+      case expression:
+
+        break;
+      default:
+
+    }
+
+    //Declare the instance function open here.
+    this.open = function(){
+      var state ="active";
+
+
+
+    };
+    //Declare the instance function sleep here.
+    this.sleep = function(){
+      if(this.state == true){
+        this.state = "sleep";
+        this.memory = this.memory/2;
+      }
+
+}
+
+
+    //Declare the instance function active here.
+
+    this.active = function(){
+      else(this.state == false)
+    }
+
+
+    //Declare the instance function close here.
+
+    this.close = function{
+    }
+
+
+}
+
+//Use the constructor to create 4 App objects below.
+
+
+
+//Declare an array named appList, and place all 4 of your apps in that array.
+//The order does not matter.
